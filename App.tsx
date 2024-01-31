2b056032ef2879b3cbee36468e320e3cf35d5fde
Francisco Ramos <franciscoramos0709@gmail.com>
2024-01-31 12:04:04 -0700

    segundo push

--- App.tsx
+++ App.tsx
@@ -1,11 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button, Pressable } from "react-native";
+import { useVocieRecognition } from "./hooks/useVoiceRecognition";
 
 export default function App() {
+  const [borderColor, setBorderColor] = useState<"lightgray" | "lightgreen">(
+    "lightgray"
+  );
+
+  const { state, startRecognizing, stopRecognizing, destroyRecognizer } =
+    useVocieRecognition();
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 32, fontWeight: "bold", marginBottom: 30 }}>
+        TalkGPT
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          color: "#333333",
+          marginVertical: 5,
+          fontSize: 12,
+        }}
+      >
+        Press and hold this button to record ypur voice. Realease to send the
+        recording, and you'll hear a response
+      </Text>
+      <Text style={{ marginVertical: 10, fontSize: 17 }}>
+        {JSON.stringify(state, null, 2)}
+      </Text>
+      <Text style={{ marginVertical: 10, fontSize: 17 }}>Your Message</Text>
+      <Pressable
+        onPressIn={() => {
+          setBorderColor("lightgreen");
+          startRecognizing();
+        }}
+        onPressOut={() => {
+          setBorderColor("lightgray");
+          stopRecognizing();
+          //handleSubmit();
+        }}
+        style={{
+          width: "90%",
+          padding: 30,
+          gap: 10,
+          borderWidth: 3,
+          alignItems: "center",
+          borderRadius: 10,
+          borderColor: borderColor,
+        }}
+      >
+        <Text>Hold to Speak</Text>
+      </Pressable>
+      <Button title="Reply Last message" onPress={() => {}} />
     </View>
   );
 }
@@ -13,8 +60,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
   },
 });
